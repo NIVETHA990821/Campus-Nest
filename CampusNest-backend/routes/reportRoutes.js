9885ff871e39dd88cdaf22b49651a107f6fd2380
NIVETHA990821 <nivethanive990821@gmail.com>
2026-05-17 15:36:52 +0530

    Update reportRoutes.js
    get all report by admin

--- CampusNest-backend/routes/reportRoutes.js
+++ CampusNest-backend/routes/reportRoutes.js
@@ -2,13 +2,19 @@ const express = require("express");
 const router = express.Router();
 const {
   createReport,
-  getReportsByListing,
+  getAllReports,
+  getReportById,
+  updateReport,
+  deleteReport,
 } = require("../controllers/reportController");
 const { protect, authorize } = require("../middleware/authMiddleware");
 
-router.get("/listing/:listingId", getReportsByListing);
-
 router.post("/:listingId", protect, authorize("student"), createReport);
-
+router.get("/", protect, authorize("admin"), getAllReports);
+router.get("/:id", protect, authorize("admin"), getReportById);
+router.put("/:id", protect, authorize("admin"), updateReport);
+router.delete("/:id", protect, authorize("admin"), deleteReport);
 
 module.exports = router;
+
+
